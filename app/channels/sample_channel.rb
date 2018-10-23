1a0e1adcea7f3001132236b36e3ed0a2f302db5b
someone <lsc182001@gmail.com>
2018-10-23 15:16:10 -0400

    add redis testing

---
+++ app/channels/sample_channel.rb
@@ -0,0 +1,9 @@
+class SampleChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "test_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+  	@message = 'someone else is here'
+  	ActionCable.server.broadcast('test_channel', msg: @message)
   end
 end
